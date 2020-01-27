c134fcbc48cb1d0583d8dfb6341dc984dfe2ab3e
Alexei M <alexeidjango@gmail.com>
2020-01-27 12:51:56 +0300

    Change run file example to explicitly set API token (to avoid confusion)

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -it \
-    --env UPTIME_API_TOKEN \
+    --env UPTIME_API_TOKEN="YOUR_API_TOKEN" \
     --mount type=bind,source=/sys/fs/cgroup,target=/sys/fs/cgroup \
     --mount type=volume,dst=/usr/local/nagios/etc/hosts,src=uptime-nagios-hosts \
     --mount type=volume,dst=/usr/local/nagios/var/,src=uptime-nagios-var \
