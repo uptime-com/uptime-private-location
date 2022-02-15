73d2b809c95f4c69496d7581084e99733c5a6978
Alexei M <alexeidjango@gmail.com>
2022-02-15 21:43:15 +0300

    Make mounts consistent between readme and runner script

--- run_example.sh
+++ run_example.sh
@@ -3,7 +3,6 @@
 docker run -it \
     --env UPTIME_API_TOKEN="YOUR_API_TOKEN" \
     --shm-size=2048m \
-    --mount type=bind,source=/sys/fs/cgroup,target=/sys/fs/cgroup \
     --mount type=volume,dst=/usr/local/nagios/etc/hosts,src=uptime-nagios-hosts \
     --mount type=volume,dst=/usr/local/nagios/var/,src=uptime-nagios-var \
     --mount type=volume,dst=/home/webapps/uptime/logs/,src=uptime-logs \
